8720bdc1d5bee9a61d7c819b75847f7d98275d4f
kphi <kphi@ecg-hq.com>
2020-11-03 14:21:55 -0700

    edit comment.py

--- comments.py
+++ comments.py
@@ -63,6 +63,36 @@ Ctrl+Shift+p > Preference: Open Settings(JSON)
 # *todo not bold: Task 1: Need to add error handling...
 # TODO BOLD: Task 2: Need to add this to next sprint.
 
+# * Install Comment Divider vs-code extension
+code --install-extension stackbreak.comment-divider
+
+# * type then Shift+Alt+x
+# ---------------------------------------------------------------------------- #
+#                                  Function 1                                  #
+# ---------------------------------------------------------------------------- #
+
+# * type then Alt+x
+# -------------------------------- Function 2 -------------------------------- #
+
+# * To customize line length
+# * Set line length for all dividers.
+# * Edit User settings.json in VScode
+Ctrl+Shift+p > Preference: Open Settings(JSON)
+  "comment-divider.length": 70,
+  "comment-divider.mainHeaderFiller": "#",
+    // "Set symbol for subheader line filling (only one).
+  "comment-divider.subHeaderFiller": "# #",
+   // Set symbol for solid line filling.
+  "comment-divider.lineFiller": "#",
+
+# * type then Shift+Alt+x
+# ################################################################## #
+#                                Test                                #
+# ################################################################## #
+# * type then Alt+x
+# ----------------------------- Test 2 ----------------------------- #
+
+
 # * Just FYI
 code --list-extensions
 code --install-extension aaron-bond.better-comments
