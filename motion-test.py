e72412ec9e5a987fcfa842b218ee3ca4dcbf3793
kphi <kphi@ecg-hq.com>
2020-11-06 15:25:48 -0700

    test

--- motion-test.py
+++ motion-test.py
@@ -1,3 +1,5 @@
-from algorithms import hello
+from algorithms import STS_Numbered , STS_Timed, TUG
 
-hello.hello_func()
+STS_Numbered.hello.hello_func()
+
+STS_Numbered.main.get_STS_Version("latest")
